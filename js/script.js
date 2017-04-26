9fc2da678451729b1b106a0fdebc1efca5d86a0b
jorgevalerio <jorgevalerio921@gmail.com>
2017-04-26 00:03:52 -0700

    Quote Generator Working

--- js/script.js
+++ js/script.js
@@ -1,79 +1,85 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
 
 let quotes = [
 	
 	{ quote: "I've found that luck is quite predictable. If you want more luck, take more chances. Be more active. Show up more often.",
 	
-	 source : "Bryan Tracy"
+	 source : "Bryan Tracy",
+	 year : "2000",
+	 citation : "Eat That Frog"
 		},
 	
 	{ quote: "Invest three percent of your income in yourself (self-development) in order to guarantee your future.",
 	
-	 source : "Bryan Tracy"
+	 source : "Bryan Tracy",
+	 year : "1998",
+	 citation : "Speak to Win"
 		},
 	
 	{ quote: "Successful people are always looking for opportunities to help others. Unsuccessful people are always asking, 'What's in it for me? ",
 	
-	 source : "Bryan Tracy"
+	 source : "Bryan Tracy",
+	 year : "2005",
+	 citation : "Get Smart"
+		},
+	
+		{ quote: "It doesn't matter where you are coming from. All that matters is where you are going.",
+	
+	 source : "Bryan Tracy",
+	 year : "2010",
+	 citation : "Million Dollar Habits"
+		},
+	
+	{ quote: "The key to success is to focus our conscious mind on things we desire not things we fear.",
+	
+	 source : "Bryan Tracy",
+	 year : "2002",
+	 citation : "Change Your Thinking"
 		}
 ];
 
 // Declaring Global Scope Variables
 
 let totalNumOfQuotes = quotes.length;
-console.log(totalNumOfQuotes); 
-
-
-
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
+let quoteBox = document.getElementById('quote-box');
 
 
 function getRandomQuote () {
 	// selects a random quote object from the quotes array
-	let randomQuote = Math.floor((Math.random() * totalNumOfQuotes) +1);
-						
-	
+	let randomQuote = Math.floor((Math.random() * totalNumOfQuotes) +1);			
 	// returns the randomly selected quote object
-	
-	let randomQuoteHolder = quotes.randomQuote;
-	
-	console.log(randomQuoteHolder);
-	return randomQuoteHolder;
-	
-	
-	
-	
-	
+	return quotes[randomQuote];
 }
 
-
-getRandomQuote();
-
-//function printQuote () {
+// PrintQuote Function
+function printQuote () {
 	
 	// printQuote calls the getRandomQuote function and stores the returned quote object in a variable
 	
+	let generatedRandomQuote = getRandomQuote();
+	
 	// printQuote constructs a string containing the different properties of the quote object using the following HTML template:
 	
-	/* <p class="quote"> [quote here] </p>
-<p class="source"> [source here]
-  <span class="citation"> [citation here] </span>
-  <span class="year"> [year here] </span>
-</p>  */
+
+	let newQuoteString = '<p class="quote">' + generatedRandomQuote.quote +'</p><p class="source">'+generatedRandomQuote.source+'<span class="citation">'+ generatedRandomQuote.citation +'</span>  <span class="year"> '+generatedRandomQuote.year+'</span></p>'
+  //<span class="citation"> [citation here] </span>
+	
+	
+	
+  //<span class="year"> [year here] </span>
+ 
 	
 	
 	// printQuote doesn't add a for a missing citation or a if the year property is missing
 	
-	//accomplish that: document.getElementById('quote-box').innerHTML
+	//accomplish that:
+	quoteBox.innerHTML = newQuoteString;
 	
 	
-//}
-
-
-
-
+}
 
 
 
