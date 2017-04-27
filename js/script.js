0b3d4cd3116a99ae52bee629e83b9858015b8a17
jorgevalerio <jorgevalerio921@gmail.com>
2017-04-27 01:02:24 -0700

    conditional staements added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
 let quotes = [
 	
-	{ quote: "I've found that luck is quite predictable. If you want more luck, take more chances. Be more active. Show up more often.",
+	{ quote: "I have found that luck is quite predictable. If you want more luck, take more chances. Be more active. Show up more often.",
 	
 	 source : "Bryan Tracy",
-	 year : "2000",
+	 year : "",
 	 citation : "Eat That Frog"
 		},
 	
@@ -16,14 +14,14 @@ let quotes = [
 	
 	 source : "Bryan Tracy",
 	 year : "1998",
-	 citation : "Speak to Win"
+	 citation : ""
 		},
 	
-	{ quote: "Successful people are always looking for opportunities to help others. Unsuccessful people are always asking, 'What's in it for me? ",
+	{ quote: "Successful people are always looking for opportunities to help others. Unsuccessful people are always asking, What is in it for me? ",
 	
 	 source : "Bryan Tracy",
-	 year : "2005",
-	 citation : "Get Smart"
+	 year : "",
+	 citation : ""
 		},
 	
 		{ quote: "It doesn't matter where you are coming from. All that matters is where you are going.",
@@ -36,7 +34,7 @@ let quotes = [
 	{ quote: "The key to success is to focus our conscious mind on things we desire not things we fear.",
 	
 	 source : "Bryan Tracy",
-	 year : "2002",
+	 year : "",
 	 citation : "Change Your Thinking"
 		}
 ];
@@ -49,7 +47,7 @@ let quoteBox = document.getElementById('quote-box');
 
 function getRandomQuote () {
 	// selects a random quote object from the quotes array
-	let randomQuote = Math.floor((Math.random() * totalNumOfQuotes) +1);			
+	let randomQuote = Math.floor(Math.random() * totalNumOfQuotes);			
 	// returns the randomly selected quote object
 	return quotes[randomQuote];
 }
@@ -58,33 +56,34 @@ function getRandomQuote () {
 function printQuote () {
 	
 	// printQuote calls the getRandomQuote function and stores the returned quote object in a variable
-	
-	let generatedRandomQuote = getRandomQuote();
-	
-	// printQuote constructs a string containing the different properties of the quote object using the following HTML template:
-	
 
-	let newQuoteString = '<p class="quote">' + generatedRandomQuote.quote +'</p><p class="source">'+generatedRandomQuote.source+'<span class="citation">'+ generatedRandomQuote.citation +'</span>  <span class="year"> '+generatedRandomQuote.year+'</span></p>'
-  //<span class="citation"> [citation here] </span>
-	
-	
-	
-  //<span class="year"> [year here] </span>
- 
-	
-	
-	// printQuote doesn't add a for a missing citation or a if the year property is missing
-	
+  let generatedRandomQuote = getRandomQuote();
+  let $quote = generatedRandomQuote.quote;
+  let $source = generatedRandomQuote.source;
+  let $citation = generatedRandomQuote.citation;
+  let $year =  generatedRandomQuote.year;
+  let $newQuote = '<p class="quote">' + $quote + '</p><p class="source">' + $source + '</span>';
+  
+  // printQuote constructs a string containing the different properties of the quote object using the following HTML template:
 	//accomplish that:
-	quoteBox.innerHTML = newQuoteString;
-	
-	
-}
+  
+  if ($citation.length === 0 && $year.length === 0) {
+			    $newQuote += '.</p>';
+	            quoteBox.innerHTML = $newQuote;
 
+			
+		  } else if ($citation.length === 0) {
+				$newQuote +=  '<span class="year"> '+$year+'.</span></p>';
+			    quoteBox.innerHTML = $newQuote;
+			 
+		  } else if ($year.length === 0) {
+				 $newQuote +=  '<span class="citation"> '+$citation+'.</span></p>';
+                 quoteBox.innerHTML = $newQuote;
+			  
+		  } else {
+			    $newQuote +=  '<span class="citation">'+ $citation +'</span>  <span class="year"> '+$year+'.</span></p>';
+                quoteBox.innerHTML = $newQuote;
+			 
+		  }
 
-
-
-
-
-
-
+	}
